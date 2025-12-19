9f0f20beb2493ef21734c29b102339e1681a833b
Harsimran12003 <harsimran12003@gmail.com>
2025-12-20 01:32:19 +0530

    updated product card

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: ["http://localhost:5173", "https://www.capitalstorecs.com/"],
     credentials: true,               
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
