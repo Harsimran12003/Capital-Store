e5951fe011f713dea3a5dc64021e1155dbc181c0
Harsimran12003 <harsimran12003@gmail.com>
2025-12-22 01:38:07 +0530

    edited reviews9

--- backend/middleware/uploadReviewImages.js
+++ backend/middleware/uploadReviewImages.js
@@ -1,8 +1,8 @@
 import multer from "multer";
-import  CloudinaryStorage  from "multer-storage-cloudinary";
+import multerStorageCloudinary from "multer-storage-cloudinary";
 import cloudinary from "../config/cloudinary.js";
 
-const storage = new CloudinaryStorage({
+const storage = new multerStorageCloudinary({
   cloudinary,
   params: {
     folder: "capitalstore/reviews",
